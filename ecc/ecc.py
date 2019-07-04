c6cc92541700630dc31eb955b82f563ec970ad44
BobleChinois <674694@protonmail.ch>
2019-07-04 14:24:35 +0200

    chapter 1 complete, all tests pass

--- ecc/ecc.py
+++ ecc/ecc.py
@@ -14,12 +14,45 @@ class FieldElement:
         self.prime = prime
 
     def __repr__(self):
-        # this overwrite the standard print() function
+        # this overwrite the standard `print()` function
         return f'FieldElement {self.prime}({self.num})'
 
     def __eq__(self, other):
-        # this overwrite the standard == operator
+        # this overwrite the standard `==` operator
         if other is None:
             return False # return an error instead of simply False?
         # you need two things to compare, if not it should fail
         return self.num == other.num and self.prime == other.prime
+
+    def __ne__(self, other):
+        # this overwrite the standard `!=` operator
+        return not (self == other)
+    
+    def __add__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot add two numbers in different Fields')
+        num = (self.num + other.num) % self.prime
+        return self.__class__(num, self.prime) # return self.__class__ means retur a new object of the same class
+
+    def __sub__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot substract two numbers in different Fields')
+        num = (self.num - other.num) % self.prime
+        return self.__class__(num, self.prime)  
+
+    def __mul__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot multiply two numbers in different Fields')
+        num = (self.num * other.num) % self.prime
+        return self.__class__(num, self.prime)  
+
+    def __pow__(self, exponent):
+        n = exponent % (self.prime - 1)
+        num = pow(self.num, n, self.prime)
+        return self.__class__(num, self.prime)
+
+    def __truediv__(self, other):
+        if self.prime != other.prime:
+            raise TypeError('Cannot divide two numbers in different Fields')
+        num = self.num * pow(other.num, (self.prime-2), self.prime) % self.prime
+        return self.__class__(num, self.prime)
--- test/test_ecc.py
+++ test/test_ecc.py
@@ -20,3 +20,58 @@ def test_error():
         c = FieldElement(-5, 23)
     with pytest.raises(ValueError):
         d = FieldElement(None, None)
+
+def test_ne():
+    a = FieldElement(2, 31)
+    b = FieldElement(2, 31)
+    c = FieldElement(15, 31)
+    assert a == b
+    assert (a != c) == True
+    assert (a != b) == False
+    
+def test_add():
+    a = FieldElement(2, 31)
+    b = FieldElement(15, 31)
+    c = FieldElement(17, 31)
+    d = FieldElement(21, 31)
+    e = FieldElement(7, 31)
+    assert (a + b) == c
+    assert (c + d) == e
+ 
+def test_sub():
+    a = FieldElement(29, 31)
+    b = FieldElement(4, 31)
+    c = FieldElement(25, 31)
+    d = FieldElement(15, 31)
+    e = FieldElement(30, 31)
+    f = FieldElement(16, 31)
+    assert (a - b) == c
+    assert (d - e) == f
+
+def test_mul():
+    a = FieldElement(24, 31)
+    b = FieldElement(19, 31)
+    c = FieldElement(22, 31)
+    assert (a * b) == c
+
+def test_pow():
+    a = FieldElement(17, 31)
+    b = FieldElement(15, 31)
+    c = FieldElement(5, 31)
+    d = FieldElement(18, 31)
+    e = FieldElement(16, 31)
+    assert a**3 == b
+    assert c**5 * d == e
+
+def test_div():
+    a = FieldElement(3, 31)
+    b = FieldElement(24, 31)
+    c = FieldElement(4, 31)
+    d = FieldElement(17, 31)
+    e = FieldElement(29, 31)
+    f = FieldElement(11, 31)
+    g = FieldElement(13, 31)
+    assert a / b == c
+    assert d**-3 == e
+    assert c**-4 * f == g
+    
