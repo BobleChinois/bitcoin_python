15cdfb0ff88a82e54db626228854c619451b3cd7
Sosthene00 <674694@protonmail.ch>
2019-10-24 18:54:46 +0200

    create class Block

---
+++ blocks/block.py
@@ -0,0 +1,34 @@
+from ecc.util import *
+
+class Block:
+
+    def __init__(self, version, prev_block, merkle_root, timestamp, bits, nonce):
+        self.version = version
+        self.prev_block = prev_block
+        self.merkle_root = merkle_root
+        self.timestamp = timestamp
+        self.bits = bits
+        self.nonce = nonce
+
+    def serialize(self):
+        result = int_to_little_endian(self.version, 4)
+        result += self.prev_block[::-1]
+        result += self.merkle_root[::-1]
+        result += int_to_little_endian(self.timestamp, 4)
+        result += self.bits
+        result += self.nonce
+        return result
+
+    def hash(self):
+        return hash256(self.serialize())[::-1]
+
+    @classmethod
+    def parse(cls, s):
+        version = little_endian_to_int(s.read(4))
+        prev_block = s.read(32)[::-1]
+        merkle_root = s.read(32)[::-1]
+        timestamp = little_endian_to_int(s.read(4))
+        bits = s.read(4)
+        nonce = s.read(4)
+        return cls(version, prev_block, merkle_root, timestamp, bits, nonce)
+
