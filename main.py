fe5e2c823f79a483132648d43aa8afa5111965eb
Sosthene00 <674694@protonmail.ch>
2020-04-14 23:26:53 +0200

    main: add an option to experiment with LNPBP1 commitment tweak

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ from tx.tx import *
 from tx.script import *
 from validate.op import *
 from os import getcwd
+from sys import argv
 
-def get_keys():
-    if input("Avez-vous une clé ?") == "Oui":
+def get_keys(key=None):
+    if key is not None:
+        return PrivateKey(key)
+    elif input("Avez-vous une clé ?") == "Oui":
         secret = int(input("Saisissez votre clé privée : "))
     else:
         secret = input("Saisissez une phrase secrète : ")
@@ -68,8 +71,24 @@ def malleate_ecdsa(m, key):
 def check_sig(m, pubkey, sig):
     return pubkey.verify(m, sig)
 
-def main():
-    keys = get_keys()
+def commitment(point):
+    domain = input("What is the domain? (ex: Taproot, LNPBP1...)")
+    protocol = input("What is the protocol?")
+    msg = input("What is the message being commited? (ex: \"it was never about a virus\")")
+    tweak = point.create_commitment(domain, protocol, msg)
+    commitment = point.tweak_pubkey(tweak.point)
+    print("The pubkey containing the commitment is ", tweak.point)
+    print("It can be checked with the original pubkey, and the msg")
+    print("Is ", tweak.point, "valid for ", point, "and", msg,
+    "?")
+    print(point.verify_commitment(domain, protocol, msg, commitment))
+    return {'tweak': tweak, 'msg': msg}
+
+def main(secret=None):
+    if len(argv) == 2:
+        keys = get_keys(int(argv[1]))
+    else:
+        keys = get_keys(None)
     dirname = getcwd()
     filename = dirname + "/my_secret"
     update_keyfile(keys, filename)
@@ -82,6 +101,7 @@ def main():
             4 voir la clé
             5 générer une nouvelle clé
             6 malléabilité de signature
+            7 Créer un commitment
             """)
         if action == "1":
             adress = keys.point.address(testnet=True)
@@ -101,13 +121,20 @@ def main():
         elif action == "5":
             main()
         elif action == "6":
-            m = input("Tapez un message à signer")
+            m = input("Type in some message to sign")
             mal_sig = malleate_ecdsa(m, keys)
             print("Is the malleated sig valid ? {}".format(check_sig(m,
             keys.point, mal_sig)))
             continue
+        elif action == "7":
+            commit_info = commitment(keys.point)
+            tweak = commit_info['tweak']
+            msg = commit_info['msg']
+            print("You can use the new key pair",\
+                tweak.secret, tweak.point, "that commits to ", msg)
+            main(tweak.secret)
         else:
-            print("Choisissez une valeur entre 1 et 6")
+            print("Choisissez une valeur entre 1 et 7")
             continue
 
 if __name__ == "__main__":
